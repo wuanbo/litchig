be8bd48d5add4cf68b3b0cccc395bcd44e932981
Jing Li <lym125@gmail.com>
2020-03-03 09:55:18 +0800

    Init account module

---
+++ Modules/Account/Entities/Model.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Modules\Account\Entities;
+
+use Illuminate\Database\Eloquent\Model as EloquentModel;
+
+abstract class Model extends EloquentModel
+{
+}
---
+++ Modules/Account/Routes/api.php
@@ -0,0 +1,7 @@
+<?php
+
+Route::group([
+    'prefix' => 'account',
+], function () {
+    // 路由配置
+});
